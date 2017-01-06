f926fd805f4eb99d731df3988c924ceeb89c02e6
Wen-Chun Lin <lovecankill@gmail.com>
2017-01-06 09:44:17 +0800

    Initial commit

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject telegraph "0.1.0-SNAPSHOT"
+  :description "Clojure Telegraph API"
+  :url "https://github.com/cataska/telegraph"
+  :license {:name "MIT License"
+            :url "https://opensource.org/licenses/MIT"}
+  :dependencies [[org.clojure/clojure "1.8.0"]])
---
+++ test/telegraph/core_test.clj
@@ -0,0 +1,7 @@
+(ns telegraph.core-test
+  (:require [clojure.test :refer :all]
+            [telegraph.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
