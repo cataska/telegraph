b88334bb2a4d2da0ebb6420b6cd8266986a8a346
Wen-Chun Lin <lovecankill@gmail.com>
2017-01-06 19:27:55 +0800

    Support createAccount and editAccountInfo api

--- project.clj
+++ project.clj
@@ -3,4 +3,6 @@
   :url "https://github.com/cataska/telegraph"
   :license {:name "MIT License"
             :url "https://opensource.org/licenses/MIT"}
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [clj-http "3.4.1"]
+                 [cheshire "5.6.3"]])
--- src/telegraph/core.clj
+++ src/telegraph/core.clj
@@ -1,6 +1,43 @@
-(ns telegraph.core)
+(ns telegraph.core
+  (:require [clj-http.client :as http]
+            [cheshire.core :refer :all]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(def api-url "https://api.telegra.ph")
+
+(defn filter-nil-val [m]
+  (into {} (filter (comp some? val) m)))
+
+(defn create-account
+  "Create a new Telegraph account"
+  ([short-name]
+   (create-account short-name nil nil))
+  ([short-name author-name]
+   (create-account short-name author-name nil))
+  ([short-name author-name, author-url]
+   (let [endpoint (str api-url "/createAccount")]
+     (->
+       (http/get endpoint
+         {:query-params (filter-nil-val {:short_name short-name
+                                         :author_name author-name
+                                         :author_url author-url})})
+       (:body)
+       (parse-string true)))))
+
+(defn str-or-nil
+  [s]
+  (if (nil? s)
+    "nil"
+    s))
+
+(defn edit-account-info
+  "Update information about a Telegraph account"
+  [token {:keys [short-name author-name author-url]}]
+  (let [endpoint (str api-url "/editAccountInfo")]
+    (->
+      (http/get endpoint
+        {:query-params (filter-nil-val {:access_token token
+                                        :short_name short-name
+                                        :author_name author-name
+                                        :author_url author-url})})
+      (:body)
+      (parse-string true))))
