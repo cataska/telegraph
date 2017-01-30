ebcb06fdf87be46d33d0dd1ffa741b9de95b71eb
Wen-Chun Lin <lovecankill@gmail.com>
2017-01-30 20:45:26 +0800

    Save my work

--- project.clj
+++ project.clj
@@ -5,4 +5,7 @@
             :url "https://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [clj-http "3.4.1"]
-                 [cheshire "5.6.3"]])
+                 [clj-tagsoup "0.3.0"]
+                 [cheshire "5.6.3"]
+                 [hickory "0.7.0"]
+                 [enlive "1.1.6"]])
